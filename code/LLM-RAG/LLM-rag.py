c4fa48951e197b63b6255310e8073ccfe4b402a6
Stephan Raaijmakers <stephan.raaijmakers@tno.nl>
2024-03-19 13:09:53 +0100

    Update

--- code/LLM-RAG/LLM-rag.py
+++ code/LLM-RAG/LLM-rag.py
@@ -42,15 +42,19 @@ Let's think step by step. Answer in Dutch.
 Answer: """
 
 # https://huggingface.co/spaces/BramVanroy/open_dutch_llm_leaderboard
-# OK llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.1, "max_length":64})
+
 
 llm = HuggingFaceHub(repo_id="HuggingFaceH4/zephyr-7b-beta", model_kwargs={"temperature":0.1,"max_length":128})
 
-# TOO BIG llm = HuggingFaceHub(repo_id="BramVanroy/Llama-2-13b-chat-dutch", model_kwargs={"temperature":0.1,"max_length":64})
-# TOO BIG llm = HuggingFaceHub(repo_id="BramVanroy/GEITje-7B-ultra", model_kwargs={"temperature":0.1,"max_length":64})
-# OK llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-v0.1", model_kwargs={"temperature":0.1, "max_length":64})
+# llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.1, "max_length":64})
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+# llm = HuggingFaceHub(repo_id="BramVanroy/Llama-2-13b-chat-dutch", model_kwargs={"temperature":0.1,"max_length":64})
+# llm = HuggingFaceHub(repo_id="BramVanroy/GEITje-7B-ultra", model_kwargs={"temperature":0.1,"max_length":64})
+# llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-v0.1", model_kwargs={"temperature":0.1, "max_length":64})
 
-#prompt = """Question: Can Barack Obama have a conversation with George Washington?
+# Replace <question> with your question below:
+
+#prompt = """Question: <question>?
 
 #Let's think step by step.
 
@@ -58,9 +62,10 @@ llm = HuggingFaceHub(repo_id="HuggingFaceH4/zephyr-7b-beta", model_kwargs={"temp
 
 #print(llm(prompt))
 
-# ===================================================================
+# =============================== DOCUMENT LOADING ====================================
 
-# Replace <url> with your url below if you want to crawl/scrape a web source.
+# Option 1: from the web
+# Replace <url> with your url below:
 loader = WebBaseLoader(
     web_paths=("<url>",),
 
@@ -71,9 +76,9 @@ loader = WebBaseLoader(
         )
     ),
 )
+#docs = loader.load()
 
-# Create a folder ./documents, with *.txt files.
-
+# Option 2: create a folder ./documents, with *.txt files.
 docs=[]
 n=0
 for file in os.listdir("./documents"):
@@ -81,27 +86,21 @@ for file in os.listdir("./documents"):
     n+=1
     loader=TextLoader("./documents/"+file)
     docs.extend(loader.load())
-#docs = loader.load()
 
 print("LOADED ",n, " documents")
-print(docs)
+
+# Process documents:
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 
-#st_emb = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
 #embedding_function = SentenceTransformerEmbeddings(model_name="GroNLP/gpt2-medium-dutch-embeddings")
 
 vectorstore = Chroma.from_documents(documents=splits,embedding=embedding_function)
 
-# Retrieve and generate using the relevant snippets of the blog.
+# ========================== Retrieve and generate ==============================
 retriever = vectorstore.as_retriever()
 prompt = hub.pull("rlm/rag-prompt")
-
-#llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
